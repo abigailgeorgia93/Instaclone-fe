9b00e3564cce9bb0faf634ec2c194b1e61327023
Samm Wiltshire <samm94tw@gmail.com>
2024-10-01 14:26:37 +0100

    enhancement: added header code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 import './Header.css';  
 
 const Header = () => {
@@ -7,9 +7,9 @@ const Header = () => {
       <h1>INSTADOG</h1>
       <nav>
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+
+          <li><Link to="/">SignUp</Link></li>
+          <li><Link to="/images">Images</Link></li>
         </ul>
       </nav>
     </header>
