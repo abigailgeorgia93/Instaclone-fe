40571101b2e4cf9ebd5fb248021014f6d09c4530
Samm Wiltshire <samm94tw@gmail.com>
2024-10-02 10:38:57 +0100

    chore: added fetch.js in utils folder

--- src/components/logOrSign/signup/Signup.jsx
+++ src/components/logOrSign/signup/Signup.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
+import { useState } from "react";
+import { signupFetch } from "../../../utils/fetch";
+import "./Signup.css";
 
-const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+const Signup = ({ setLoggedUser, setIsLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (e, setter) => {
+    setter(e.target.value);
+  };
+
+  const testSign = (username, email, password) => {
+    console.log("test is working");
+    console.log(username, email, password);
+    return {
+      message: "success",
+      user: {
+        id: 1,
+        username: "michael",
+      },
+    };
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = await signupFetch(username, email, password);
+
+
+if (data.message === "success"){
+      setLoggedUser(data);
+      setIsLoggedIn(true);
 }
+  };
 
-export default Signup
+  return (
+    <div className="flex flex-column signup">
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        className="flex flex-column signup-form"
+      >
+        <h3>Signup</h3>
+        <div>
+          <input
+            onChange={(e) => handleChange(e, setUsername)}
+            type="text"
+            placeholder="Username..."
+          />
+          <input
+            onChange={(e) => handleChange(e, setEmail)}
+            type="email"
+            placeholder="Email..."
+          />
+          <input
+            onChange={(e) => handleChange(e, setPassword)}
+            type="text"
+            placeholder="Password..."
+          />
+        </div>
+        <button>Signup</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
