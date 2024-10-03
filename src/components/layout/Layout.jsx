c29e2d3a1deb6f832ff77f52843f5111bb0f45a7
Daniel Worthington <daniel.worthington96@outlook.com>
2024-10-03 12:27:34 +0100

    bug-fix: duplicate code

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,22 +5,11 @@ import "./Layout.css";
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Header/>
-      {children}
-
-import Header from '../header/Header';
-import Footer from '../footer/Footer';
-import './Layout.css';
-
-const Layout = ({ children }) => {
-  return (
     <div className="layout-container">
       <Header />
       <main className="main-content">
         {children}
       </main>
-
       <Footer />
     </div>
   );
